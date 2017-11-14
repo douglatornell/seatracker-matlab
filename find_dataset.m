8985dba2db980d75f41f94cc3cc2c0147fcaebe5
Susan Allen <sallen@eos.ubc.ca>
2017-11-14 13:26:20 -0800

    Fix strings to work pre 2017

--- find_dataset.m
+++ find_dataset.m
@@ -1,16 +1,16 @@
 function [udataset, vdataset, wdataset, tdataset, useindex] = find_dataset(nextindex)
-    stem1="SS_velocity_";
-    stem2="SS_ssh_";
+    stem1='SS_velocity_';
+    stem2='SS_ssh_';
     if nextindex <= 24
-        datefield = "17apr16";
+        datefield = '09apr16';
         useindex = nextindex
     elseif nextindex <= 48;
-        datefield = "18apr16";
+        datefield = '10apr16';
         useindex = nextindex-24;
     else
         'problem, out of data'
     end
-    udataset = stem1+datefield+"_U.nc";
-    vdataset = stem1+datefield+"_V.nc";
-    wdataset = stem1+datefield+"_W.nc";
-    tdataset = stem2+datefield+"_T.nc";
+    udataset = strcat(stem1,datefield,'_U.nc');
+    vdataset = strcat(stem1,datefield,'_V.nc');
+    wdataset = strcat(stem1,datefield,'_W.nc');
+    tdataset = strcat(stem2,datefield,'_T.nc');
